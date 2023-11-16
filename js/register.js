873b10e7f8531dd58a027d9d0b8b7074edad24ce
anhi1 <anhiemolina@gmail.com>
2023-11-16 12:26:44 +0100

    registro

--- js/register.js
+++ js/register.js
@@ -12,11 +12,9 @@ toggle.onclick = () => {
 const formulario= document.getElementById('formulario')
 const inputs = document.querySelectorAll('#formulario input')
 const expresiones = { /*expresiones es un objeto / expresion regular*/
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo que tenga un minimo de 4 carcaters y un maximo de 16
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
 const campos = {
